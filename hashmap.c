a2f1da52aeaa978781bc9928ba7ae47cb6d68254
Replit user <>
2023-05-10 18:09:23 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -53,7 +53,7 @@ void enlarge(HashMap * map) {
 
 HashMap * createMap(long capacity) {
   HashMap *mapa = (HashMap*)malloc(sizeof(HashMap));
-  mapa->buckets = NULL;
+  mapa->buckets[] = NULL;
   mapa->capacity = capacity;
   mapa->size = 0;
   mapa->current = -1;
